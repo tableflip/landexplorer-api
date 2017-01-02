32bd42afc988895cbce669eb77b27731e12714de
Oli Evans <oli@zilla.org.uk>
2017-01-02 10:23:07 +0000

    Tweak init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -15,8 +15,14 @@
 # - http://revenant.ca/www/postgis/workshop/indexing.html
 # - https://github.com/psd/landregistry-inspire-data/blob/1c239d0299d8b8c72bf48b854ce03155213b3281/Makefile#L21
 # - http://linuxcommand.org/wss0150.php
+# - http://stackoverflow.com/questions/760210/how-do-you-create-a-read-only-user-in-postgresql
 
-# Usage: parallel ./init.sh {} < files.txt
+#
+# Usage:
+# (As a user with super user db priviledges)
+# ./init.sh --create-db
+# parallel ./init.sh {} < files.txt
+# ./init.sh --index
 
 PROGNAME=$(basename $0)
 ZIP=$1
@@ -31,6 +37,7 @@ SOURCE_SRS="EPSG:27700"
 TARGET_SRS="EPSG:4326"
 
 # SQL to init postgis, schema and table.
+# Note: Only a db superuser can create the postgis extention.
 # Note: this is a `heredoc`. It'll read the multiline sql into the SQL var.)
 read -r -d '' SQL << EOM
 CREATE EXTENSION IF NOT EXISTS postgis;
@@ -40,9 +47,11 @@ CREATE TABLE IF NOT EXISTS $SCHEMA.$TABLE (
   $GEO_COLUMN_NAME geography(Polygon,4326) NOT NULL,
   inspireid integer NOT NULL
 );
+GRANT CONNECT ON DATABASE $DB TO "$DB_USER";
 GRANT USAGE ON SCHEMA $SCHEMA TO "$DB_USER";
 GRANT SELECT ON ALL TABLES IN SCHEMA $SCHEMA TO "$DB_USER";
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA $SCHEMA TO "$DB_USER";
+ALTER DEFAULT PRIVILEGES IN SCHEMA $SCHEMA GRANT SELECT ON TABLES TO "$DB_USER";
 EOM
 
 function error_exit {
@@ -50,13 +59,16 @@ function error_exit {
   exit 1
 }
 
-# Run before hand to create db and schema
-if [[ $1 = "--create-db" ]]; then
-  # If landexplorer-api user is missing, create them.
+if [[ $1 = "--create-db-user" ]]; then
+  # If $DB_USER is missing, create them. Run as db super user
   if ! psql -d postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$DB_USER'" | grep -q 1; then
-    createuser $DB_USER
-  fi;
+    createuser || error_exit "$LINENO: Failed to create user $DB_USER";
+  fi
+  echo "Done!";
+  exit 1;
+fi
 
+if [[ $1 = "--create-db" ]]; then
   # If DB missing, then create db, scheam and table.
   if [[ -z `psql -Atqc "\list $DB"` ]]; then
     createdb $DB || error_exit "$LINENO: Failed to create db $DB";
@@ -64,7 +76,6 @@ if [[ $1 = "--create-db" ]]; then
     psql -d $DB -Atqc "$SQL" || error_exit "$LINENO: Failed to create db $SCHEMA and $TABLE";
     echo "$SCHEMA.$TABLE created";
   fi
-  
   echo "Done!";
   exit 1;
 fi
@@ -115,7 +126,7 @@ fi
 
 echo "Importing $NAME.sql to $DB db"
 
-if psql -d $DB -f $NAME.sql ; then
+if psql -d $DB -f $NAME.sql -v ON_ERROR_STOP=1 ; then
   # Clean up.
   rm $NAME.sql $NAME.gml $NAME.gfs $NAME.zip
   echo "Success! $NAME.sql imported";
